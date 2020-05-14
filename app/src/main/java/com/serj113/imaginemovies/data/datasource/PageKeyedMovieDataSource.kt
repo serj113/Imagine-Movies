13da616a7610db7b45defc8cf19cddb5ee4e34b3
Setia Simaremare <setia_simaremare@yahoo.com>
2020-05-14 22:21:10 +0700

    update move source

--- app/src/main/java/com/serj113/imaginemovies/data/datasource/PageKeyedMovieDataSource.kt
+++ app/src/main/java/com/serj113/imaginemovies/data/datasource/PageKeyedMovieDataSource.kt
@@ -2,20 +2,21 @@ package com.serj113.imaginemovies.data.datasource
 
 import androidx.paging.PageKeyedDataSource
 import com.serj113.imaginemovies.data.model.Response
+import com.serj113.imaginemovies.domain.entity.Movie
 
-class PageKeyedMovieDataSource : PageKeyedDataSource<Int, Response>() {
+class PageKeyedMovieDataSource : PageKeyedDataSource<Int, Movie>() {
     override fun loadInitial(
         params: LoadInitialParams<Int>,
-        callback: LoadInitialCallback<Int, Response>
+        callback: LoadInitialCallback<Int, Movie>
     ) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Response>) {
+    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Movie>) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Response>) {
+    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Movie>) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }

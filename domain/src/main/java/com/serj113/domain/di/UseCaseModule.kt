b02186f662926025b85a6010c230ce2a35e0f729
Setia <setia_simaremare@yahoo.com>
2020-06-20 20:09:43 +0700

    migrated to hilt

--- domain/src/main/java/com/serj113/domain/di/UseCaseModule.kt
+++ domain/src/main/java/com/serj113/domain/di/UseCaseModule.kt
@@ -4,6 +4,8 @@ import com.serj113.domain.interactor.FetchMovieUseCase
 import com.serj113.domain.usecase.FetchMovieUseCaseImpl
 import dagger.Binds
 import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
 
 @InstallIn(ApplicationComponent::class)
 @Module

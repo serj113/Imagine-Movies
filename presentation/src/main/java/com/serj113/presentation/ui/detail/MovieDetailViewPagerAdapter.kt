9d705a7b3ced584ec45f29dc357720a99098998c
Setia <setia_simaremare@yahoo.com>
2020-07-12 23:58:39 +0700

    added adapter

--- presentation/src/main/java/com/serj113/presentation/ui/detail/MovieDetailViewPagerAdapter.kt
+++ presentation/src/main/java/com/serj113/presentation/ui/detail/MovieDetailViewPagerAdapter.kt
@@ -1,5 +1,18 @@
 package com.serj113.presentation.ui.detail
 
-class MovieDetailViewPagerAdapter {
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 
+class MovieDetailViewPagerAdapter(private val context: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    override fun getItem(position: Int): Fragment {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
 }

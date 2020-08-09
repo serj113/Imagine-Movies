b38aa8c93338954af5869cc42b94296e1d438713
Setia <setia_simaremare@yahoo.com>
2020-08-10 01:39:01 +0700

    update info with movie details and update each fragment to load from data binding

--- presentation/src/main/java/com/serj113/presentation/detail/InfoFragment.kt
+++ presentation/src/main/java/com/serj113/presentation/detail/InfoFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.serj113.presentation.R
+import com.serj113.presentation.databinding.FragmentInfoBinding
 
 class InfoFragment : Fragment() {
+
+    private lateinit var binding: FragmentInfoBinding
+    private val viewModel: MovieDetailViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_info, container, false)
+        binding = FragmentInfoBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
--- presentation/src/main/java/com/serj113/presentation/detail/ReviewFragment.kt
+++ presentation/src/main/java/com/serj113/presentation/detail/ReviewFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.serj113.presentation.R
+import com.serj113.presentation.databinding.FragmentReviewBinding
 
 class ReviewFragment : Fragment() {
+
+    private lateinit var binding: FragmentReviewBinding
+    private val viewModel: MovieDetailViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_review, container, false)
+        binding = FragmentReviewBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }

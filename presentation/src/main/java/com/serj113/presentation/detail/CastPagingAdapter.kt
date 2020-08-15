2dbfcf5c006b94134d4b4ae8a42b18d74ea6a14e
Setia <setia_simaremare@yahoo.com>
2020-08-15 13:07:08 +0700

    added cast adapter

--- presentation/src/main/java/com/serj113/presentation/detail/CastPagingAdapter.kt
+++ presentation/src/main/java/com/serj113/presentation/detail/CastPagingAdapter.kt
@@ -1,3 +1,36 @@
 package com.serj113.presentation.detail
 
-class CastPagingAdapter
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.serj113.domain.entity.Cast
+import com.serj113.presentation.databinding.CastListItemBinding
+
+class CastPagingAdapter :
+    PagingDataAdapter<Cast, CastPagingAdapter.CastItemViewHolder>(CastItemCallback) {
+
+    override fun onBindViewHolder(holder: CastItemViewHolder, position: Int) {
+        getItem(position)?.let { cast ->
+            holder.bind(cast)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CastItemViewHolder {
+        return CastItemViewHolder(
+            CastListItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    inner class CastItemViewHolder(
+        private val binding: CastListItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(cast: Cast) {
+
+        }
+    }
+}

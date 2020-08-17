4b8df81981799642d21783d66db3321d3232b5c0
Setia <setia_simaremare@yahoo.com>
2020-08-18 00:52:17 +0700

    added cast rows

--- presentation/src/main/java/com/serj113/presentation/detail/CastFragment.kt
+++ presentation/src/main/java/com/serj113/presentation/detail/CastFragment.kt
@@ -1,5 +1,6 @@
 package com.serj113.presentation.detail
 
+import android.graphics.drawable.ClipDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,19 +8,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.serj113.presentation.databinding.FragmentCastBinding
 
 class CastFragment : Fragment() {
 
     private lateinit var binding: FragmentCastBinding
-    private val viewModel: MovieDetailViewModel by activityViewModels()
     private val adapter = CastRecyclerViewAdapter()
+    private val viewModel: MovieDetailViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentCastBinding.inflate(inflater, container, false)
+//        adapter = CastRecyclerViewAdapter()
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView
+            .addItemDecoration(DividerItemDecoration(context, ClipDrawable.HORIZONTAL))
         binding.recyclerView.adapter = adapter
         return binding.root
     }

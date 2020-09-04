622ccc25415726bc52f0a78fa6f19001a4eea202
Setia <setia_simaremare@yahoo.com>
2020-09-04 22:03:31 +0700

    update style cast and review row

--- presentation/src/main/java/com/serj113/presentation/detail/cast/CastFragment.kt
+++ presentation/src/main/java/com/serj113/presentation/detail/cast/CastFragment.kt
@@ -1,6 +1,5 @@
 package com.serj113.presentation.detail.cast
 
-import android.graphics.drawable.ClipDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.serj113.presentation.databinding.FragmentCastBinding
 import com.serj113.presentation.detail.MovieDetailViewModel
@@ -26,8 +24,6 @@ class CastFragment : Fragment() {
     ): View? {
         _binding = FragmentCastBinding.inflate(inflater, container, false)
         binding?.recyclerView?.layoutManager = LinearLayoutManager(requireContext())
-        binding?.recyclerView
-            ?.addItemDecoration(DividerItemDecoration(context, ClipDrawable.HORIZONTAL))
         binding?.recyclerView?.adapter = adapter
         return binding?.root
     }
--- presentation/src/main/java/com/serj113/presentation/detail/review/ReviewFragment.kt
+++ presentation/src/main/java/com/serj113/presentation/detail/review/ReviewFragment.kt
@@ -1,6 +1,5 @@
 package com.serj113.presentation.detail.review
 
-import android.graphics.drawable.ClipDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,7 +8,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.serj113.domain.base.Entity
 import com.serj113.presentation.adapter.ListLoadStateAdapter
@@ -31,8 +29,6 @@ class ReviewFragment : Fragment() {
         _binding = FragmentReviewBinding.inflate(inflater, container, false)
         adapter = ReviewPagingAdapter()
         binding?.recyclerView?.layoutManager = LinearLayoutManager(requireContext())
-        binding?.recyclerView
-            ?.addItemDecoration(DividerItemDecoration(context, ClipDrawable.HORIZONTAL))
         binding?.recyclerView?.adapter = adapter?.withLoadStateFooter(
             ListLoadStateAdapter(this::onClickRetry)
         )

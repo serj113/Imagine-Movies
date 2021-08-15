83f86bc6f89e64ac1bbfda19eac838835e57f85d
Setia <setia_simaremare@yahoo.com>
2021-08-15 19:26:50 +0700

    rework home list

--- presentation/presentation_list/src/main/java/com/serj113/presentation/list/MovieListViewState.kt
+++ presentation/presentation_list/src/main/java/com/serj113/presentation/list/MovieListViewState.kt
@@ -1,10 +1,9 @@
 package com.serj113.presentation.list
 
-import androidx.paging.PagingData
 import com.serj113.model.Movie
 
 sealed class MovieListViewState {
-    data class Success(val data: PagingData<Movie>?) : MovieListViewState()
+    data class Success(val data: List<Movie>) : MovieListViewState()
     object Loading : MovieListViewState()
     data class Error(var error: Throwable) : MovieListViewState()
 }

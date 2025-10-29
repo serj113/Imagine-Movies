acfe785daaf2506f7eae142327c6ed0915d688f9
Setia Simaremare <putra.simaremare@gmail.com>
2025-10-29 22:17:52 +0200

    feat: update leak canary

--- data/src/debug/java/com/serj113/data/di/DebugOkHttpClientModule.kt
+++ data/src/debug/java/com/serj113/data/di/DebugOkHttpClientModule.kt
@@ -1,20 +1,20 @@
 package com.serj113.data.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 class DebugOkHttpClientModule {
-//    @Provides
-//    @Singleton
-//    internal fun provideOkHttpClient(
-//        flipperOkhttpInterceptor: FlipperOkhttpInterceptor
-//    ): OkHttpClient {
-//        return OkHttpClient
-//            .Builder()
-//            .addNetworkInterceptor(flipperOkhttpInterceptor)
-//            .build()
-//    }
+    @Provides
+    @Singleton
+    internal fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient
+            .Builder()
+            .build()
+    }
 }
